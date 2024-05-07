464954b45fd692be7f17064a937242819415e1e2
EdosRepsac <157375072+EdosRepsac@users.noreply.github.com>
2024-05-07 20:47:33 +0200

    Support ticket overview changes

--- Repository/GetData.cs
+++ Repository/GetData.cs
@@ -25,10 +25,28 @@ namespace ZBC.Repository
                 var dbContext = scope.ServiceProvider.GetRequiredService<CJDBContext>();
 
                 var query = from t in dbContext.Tickets
+                            join b in dbContext.Brugeres on t.BrugerId equals b.BrugerId
+                            join s in dbContext.Brugeres on t.SupporterId equals s.BrugerId
+                            join st in dbContext.Statuses on t.StatusId equals st.StatusId
+                            join p in dbContext.Prioriteters on t.PrioritetId equals p.PrioritetId
+                            join k in dbContext.Kategoriers on t.KategoriId equals k.KategoriId
                             select new TicketOverviewViewModel
                             {
                                 TicketID = t.TicketId,
-                                Titel = t.Titel
+                                Oprettelsestid = t.Oprettelsestid,
+                                Titel = t.Titel,
+                                Beskrivelse = t.Beskrivelse,
+                                BrugerFNavn = b.Fornavn,
+                                BrugerLNavn = b.Efternavn,
+                                SupportFNavn = s.Fornavn,
+                                SupportLNavn = s.Efternavn,
+                                StatusNavn = st.Navn,
+                                PrioritetNavn = p.Navn,
+                                KategoriNavn = k.Navn
+                                //Titel = t.Titel,
+                                //Beskrivelse = t.Beskrivelse,
+                                //BrugerId = t.BrugerId,
+                                //SupporterId = t.SupporterId
                             };
                 List<TicketOverviewViewModel> result = await query.ToListAsync();
                 return result;
--- ViewModels/TicketOverviewViewModel.cs
+++ ViewModels/TicketOverviewViewModel.cs
@@ -1,8 +1,28 @@
-﻿namespace ZBC.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ZBC.Models;
+
+namespace ZBC.ViewModels
 {
     public class TicketOverviewViewModel
     {
         public int TicketID { get; set; }
-        public string Titel { get; set;}
+        public DateTime? Oprettelsestid {  get; set; }
+        public string? Titel { get; set;}
+        public string? Beskrivelse { get; set; }
+        public string? BrugerFNavn { get; set; }
+        public string? BrugerLNavn { get; set; }
+        public string? SupportFNavn { get; set; }
+        public string? SupportLNavn { get; set; }
+        public string? StatusNavn { get; set; }
+        public string? PrioritetNavn { get; set; }
+        public DateTime? SidsteOpdatering { get; set; }
+        public DateTime? LukkeTid { get; set; }
+        public string? KategoriNavn { get; set; }
+        public virtual Brugere Bruger { get; set; } = null!;
+        public virtual Kategorier Kategori { get; set; } = null!;
+        public virtual ICollection<Kommentarer> Kommentarers { get; set; } = new List<Kommentarer>();
+        public virtual Prioriteter? PrioritetNavigation { get; set; }
+        public virtual Status? Status { get; set; }
+        public virtual Brugere? Supporter { get; set; }
     }
 }
